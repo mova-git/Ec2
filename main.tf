e735fa5978660b0077997f7e21437029b88298cb
mauri <abc@gmail.com>
2025-07-02 19:29:13 +0530

    ec2git

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region     = "ap-south-1"
+}
+
+resource "aws_instance" "git" {
+  ami           = "ami-0f918f7e67a3323f0"  # Amazon Linux 2
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-EC2git"
+  }
+}
---
+++ output.tf
@@ -0,0 +1,3 @@
+output "instance_public_ip" {
+  value = aws_instance.git.public_ip
+}
