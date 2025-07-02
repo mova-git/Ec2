07d9c02701828b5862fa4616759111e99700a692
mova-git <mauriya1999@gmail.com>
2025-07-02 19:48:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,16 @@
-provider "aws" {
-  region     = "ap-south-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
 
 resource "aws_instance" "git" {
   ami           = "ami-0f918f7e67a3323f0"  # Amazon Linux 2
